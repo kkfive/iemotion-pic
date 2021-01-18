a76d075f927029da650089bcd06a9f1e82161acc
XiaoKang <dreamytzk@outlook.com>
2021-01-18 19:01:12 +0800

    📦 添加小黑盒表情包

---
+++ version.js
@@ -0,0 +1,10 @@
+/**
+ * @description:
+ * @author: 小康
+ * @url: https://xiaokang.me
+ * @Date: 2021-01-18 17:51:04
+ * @LastEditTime: 2021-01-18 17:51:05
+ * @LastEditors: 小康
+ */
+const version = require('./package.json')
+console.log(version.version)
